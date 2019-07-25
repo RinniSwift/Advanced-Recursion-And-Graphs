68162af84a7ca61b138be5f6513d5ecb08f19f18
Rinni Swift <rinradaswift@gmail.com>
2019-07-24 20:37:41 -0700

    cleaned up code

---
+++ graph.py
@@ -0,0 +1,125 @@
+
+'''
+Properties
+
+Vertex: vertex object containing an id and it's neighbors
+
+    id: string representation of vertex
+    neighbors: set of vertices adjacent to self
+
+Graph: a graph class containing multiple vertexes and paths
+    
+    vertices: [Vertex] list of all vertices in the graph
+    order: total number of vertices in the graph
+    edges: [Vertex: [(Vertex, weight)]] a dict of keys being the vertex and value being vertex it connects to with a weight in a tuple format.
+
+'''
+
+
+class Vertex(object):
+
+    def __init__(self, vertex):
+        self.id = vertex
+        self.neighbors = {}
+
+    def add_neighbor(self, vertex, weight=1):
+        """Add a neighbor along with a weighted edge """
+
+        self.neighbors[vertex] = weight
+
+    def __str__(self):
+        """Output the list of neighbors of this vertex."""
+        return f'vertex "{self.id}" adjacent to {[ x.id for x in self.neighbors]}'
+
+    def get_neighbors(self):
+        """Return the neighbors of this vertex."""
+        return self.neighbors
+
+    def get_edge_weight(self, vertex):
+        """Return the weight of this edge."""
+        return self.neighbors[vertex]
+
+
+
+class Graph:
+
+    def __init__(self):
+        self.vertices = set()
+        self.order = 0
+
+    def add_vertex(self, v):
+        """Add a new vertex object to the graph with the given key and return the vertex """
+
+        self.vertices.add(v)
+        self.order += 1
+        return v
+
+    def get_vertex(self, v):
+        """Return the vertex if it exists """
+
+        # given a vertex object, return the vertex if it is in the graph
+        if v in self.vertices:
+            return v
+
+    def add_edge(self, from_vert, to_vert, cost=0):
+        """add an edge from the given vertex to the other given vertex with a cost """
+
+        if from_vert in self.vertices:
+            # adding edge between existing vertices
+            if to_vert in self.vertices:
+                from_vert.add_neighbor(to_vert, cost)
+            # adding edges between an existing node to a new node
+            else:
+                self.vertices.add(to_vert)
+                from_vert.add_neighbor(to_vert, cost)
+
+    def get_vertices(self):
+        """return all the vertices in the graph """
+        return self.vertices
+
+
+
+if __name__ == "__main__":
+
+    # Challenge 1: Create the graph
+
+
+    # create friend vertex objects
+
+    friendOne = Vertex("one")
+    friendTwo = Vertex("two")
+    friendThree = Vertex("three")
+    friendFour = Vertex("four")
+
+
+    # create empty graph
+
+    g = Graph()
+
+
+    # add friends
+
+    g.add_vertex(friendOne)
+    g.add_vertex(friendTwo)
+    g.add_vertex(friendThree)
+    g.add_vertex(friendFour)
+
+    # Add connections (non weighted edges for now)
+
+    g.add_edge(friendOne, friendTwo)
+    g.add_edge(friendTwo, friendThree)
+    g.add_edge(friendOne, friendFour)
+
+
+    # Challenge 1: Output the vertices & edges
+    # Print vertices
+
+    print(f"The vertices are: {[x.id for x in g.vertices] }")
+    print(friendOne)
+
+
+
+
+
+
+
