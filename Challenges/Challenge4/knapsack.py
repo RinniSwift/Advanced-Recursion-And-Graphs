b5a27eeb7e2bc192d65b9022a716359bd538f047
Rinni Swift <rinradaswift@gmail.com>
2019-08-06 20:20:23 -0700

    added comments

--- Challenges/Challenge4/knapsack.py
+++ Challenges/Challenge4/knapsack.py
@@ -1,5 +1,8 @@
 
 class Item():
+	'''
+		Item object containing a name, weight, and value correlated to the item. This object represents items in the knapsack.
+	'''
 
 	def __init__(self, name, w, v):
 		self.name = name
@@ -21,21 +24,26 @@ def knapsack(C, items):
 	    Returns: max value
 	'''
 	
-
+	# if items is empty or capacity is down to 0, can't add any other items so we return a tuple where at [0] is the value and at [1] is the list of items in the knapsack
 	if len(items) == 0 or C == 0:
 		return (0, [])
 
-
+	# grab first item in the items list
 	item = items[0]
 
+	# if the item's weight is greater than the capacity, call the fucntion again without this item contained in
 	if item.weight > C:
 		return knapsack(C, items[1:])
 
+	# by this point, the item can be added into the knapsack without exceeding the capacity
+	# calculate the total value containing the item
 	val_with_item, items_used_with = knapsack(C - item.weight, items[1:])
 	val_with_item += item.value
 
+	# calculate the total value without containing the item
 	val_without_item, items_used_without = knapsack(C, items[1:])
 
+	# if the value with the item is greater, than the item without, add to the total items in the knapsack list 
 	if val_with_item >= val_without_item:
 		items_used_with.append(item.name)
 		return (val_with_item, items_used_with)
