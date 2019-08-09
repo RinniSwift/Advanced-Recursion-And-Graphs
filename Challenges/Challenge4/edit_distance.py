e033b74808d2072dfddf644ab63244452af50e43
Rinni Swift <rinradaswift@gmail.com>
2019-08-09 14:22:35 -0700

    fixed

--- Challenges/Challenge4/edit_distance.py
+++ Challenges/Challenge4/edit_distance.py
@@ -26,28 +26,33 @@ edit_distance(str_1, str_2, len(str_1), len(str_2))
 
 def editDistDP(str1, str2, m, n): 
     # Create a table to store results of subproblems 
-    dp = [[0 for x in range(n+1)] for x in range(m+1)] 
+	dp = [[0 for x in range(n+1)] for x in range(m+1)] 
   
-    # Fill d[][] in bottom up manner 
-    for i in range(m+1):
-        for j in range(n+1):
+	# Fill d[][] in bottom up manner 
+	for i in range(m+1):
+		for j in range(n+1):
 
-            if i == 0: 
-                dp[i][j] = j
+			if i == 0: 
+				dp[i][j] = j
 
-            elif j == 0: 
-                dp[i][j] = i
+			elif j == 0: 
+				dp[i][j] = i
 
-            elif str1[i-1] == str2[j-1]: 
-                dp[i][j] = dp[i-1][j-1] 
+			elif str1[i-1] == str2[j-1]: 
+				dp[i][j] = dp[i-1][j-1] 
   
-            else: 
-            	i = 1, j = 2
+			else:
 
-                dp[i][j] = 1 + min(dp[i][j-1],        # Insert 
-                                   dp[i-1][j],        # Remove 
-                                   dp[i-1][j-1])    # Replace 
-  
-    return dp[m][n]
+				dp[i][j] = 1 + min(dp[i][j-1], # insert
+								dp[i-1][j],	# remove
+								dp[i-1][j-1])  # replace
+
+	return dp[m][n]
 
 editDistDP(str1, str2, len(str1), len(str2))
+
+
+
+
+
+# src: https://www.geeksforgeeks.org/edit-distance-dp-5/
