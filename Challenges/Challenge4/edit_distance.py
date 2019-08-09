f87dbca165b3a461c42c7839774bf526dfc870a5
Rinni Swift <rinradaswift@gmail.com>
2019-08-09 14:18:54 -0700

    added memoization

--- Challenges/Challenge4/edit_distance.py
+++ Challenges/Challenge4/edit_distance.py
@@ -14,10 +14,40 @@ def edit_distance(str_1, str_2, m, n):
 				   edit_distance(str_1, str_2, m, n - 1))
 
 
-str1 = "happy"
-str2 = "happpy"
-print(edit_distance(str1, str2, len(str1), len(str2)))
+str1 = "cat"
+str2 = "sweat"
+edit_distance(str1, str2, len(str1), len(str2))
 
 str_1 = "sunday"
 str_2 = "saturday"
-print(edit_distance(str_1, str_2, len(str_1), len(str_2)))
+edit_distance(str_1, str_2, len(str_1), len(str_2))
+
+
+
+def editDistDP(str1, str2, m, n): 
+    # Create a table to store results of subproblems 
+    dp = [[0 for x in range(n+1)] for x in range(m+1)] 
+  
+    # Fill d[][] in bottom up manner 
+    for i in range(m+1):
+        for j in range(n+1):
+
+            if i == 0: 
+                dp[i][j] = j
+
+            elif j == 0: 
+                dp[i][j] = i
+
+            elif str1[i-1] == str2[j-1]: 
+                dp[i][j] = dp[i-1][j-1] 
+  
+            else: 
+            	i = 1, j = 2
+
+                dp[i][j] = 1 + min(dp[i][j-1],        # Insert 
+                                   dp[i-1][j],        # Remove 
+                                   dp[i-1][j-1])    # Replace 
+  
+    return dp[m][n]
+
+editDistDP(str1, str2, len(str1), len(str2))
